fadbb866c7d6a7808762badcb5797b66eaf669f7
Simone-Fratini <simone@fratini.com>
2025-04-02 17:09:00 +0200

    Add initial project structure with Laravel, Tailwind CSS, and comic book data

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,50 @@
+<footer class="bg-yellow-300 w-full flex justify-center">
+    <div class="container flex pt-10 pb-10">
+        <div class="flex flex-col gap-4">
+            <div class="uppercase font-bold text-xl">dc comics</div>
+            <ul class="text-gray-500">
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+    
+            <div class="uppercase font-bold text-xl mt-8">shop</div>
+            <ul class="text-gray-500">
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+        </div>
+        
+        <div class="flex flex-col gap-4 ml-8">
+            <div class="uppercase font-bold text-xl">dc</div>
+            <ul class="text-gray-500">
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        
+        <div class="flex flex-col gap-4 ml-8">
+            <div class="uppercase font-bold text-xl">sites</div>
+            <ul class="text-gray-500">
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header class="bg-gray-600 w-full flex justify-center">
+    <div class="container bg-amber-300 text-white h-20 flex items-center">
+        <div class="flex justify-between w-full">
+            
+            <div class="">
+                <div>ciao</div>
+            </div>
+    
+            <nav class="flex items-center">
+                <ul class="flex gap-5 text-sm">
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/characters">characters</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/comics">comics</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/movies">movies</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/tv">tv</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/games">games</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/collectibles">collectibles</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/videos">videos</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/fans">fans</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/news">news</a></li>
+                    <li class="text-black uppercase hover:text-blue-600 transition-all font-semibold"><a href="/shop">shop</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
